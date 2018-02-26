f6a98834b65926191b4149bebea669aa55f9deaa
Johannes Siedersberger <36839962+johannessiedersberger@users.noreply.github.com>
2018-02-26 13:33:28 +0100

    Add files via upload

---
+++ DoublyLinkedList/DoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleLinkedList
+{
+  public class DoublyLinkedList<T>
+  {
+    private class Node
+    {
+      public Node Next;
+      public Node Prev;
+      public T Data;
+    }
+
+    private Node _topElement;
+
+    /// <summary>
+    /// Adds a new Value at the end of the list
+    /// </summary>
+    /// <param name="value"></param>
+    public void Add(T value)
+    {
+      if (_topElement == null)
+      {
+        _topElement = new Node()
+        {
+          Data = value
+        };
+      }
+      else
+      {
+        Node last = GetLast();
+
+        last.Next = new Node()
+        {
+          Data = value,
+          Prev = last
+        };
+      }
+    }
+
+    private Node GetLast()
+    {
+      Node current = _topElement;
+      while (current.Next != null)
+        current = current.Next;
+
+      return current;
+    }
+
+    /// <summary>
+    /// Checks if the given value is included in the list
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public bool Exists(T value)
+    {
+      for (Node current = _topElement; current != null; current = current.Next)
+      {
+        if (current.Data.Equals(value))
+          return true;
+      }
+      return false;
+    }
+
+    /// <summary>
+    /// Returns the lenght of the list
+    /// </summary>
+    /// <returns></returns>
+    public int Length()
+    {
+      int length = 0;
+      for (Node current = _topElement; current != null; current = current.Next)
+      {
+        length++;
+      }
+      return length;
+    }
+
+    /// <summary>
+    /// Returns the Value of the Index in the list
+    /// </summary>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    public T Get(int index)
+    {
+      if (index < 0)
+        throw new ArgumentException("The index is not in the list");
+
+      Node current = _topElement;
+      for (int i = 0; i < index && current != null; i++)
+      {
+        current = current.Next;
+      }
+
+      if (current == null)
+        throw new ArgumentException("The index is not in the list");
+
+      return current.Data;
+    }
+
+    /// <summary>
+    /// Removes the given value from the list
+    /// </summary>
+    /// <param name="value"></param>
+    public void Remove(T value)
+    {
+      for (Node current = _topElement; current != null; current = current.Next)
+      {
+        if (_topElement.Data.Equals(value))
+        {
+          _topElement = current.Next;
+        }
+        else if (current.Next != null && current.Next.Data.Equals(value))
+        {
+          current.Next = current.Next.Next;
+        }
+        if (current.Prev != null && current.Prev.Data.Equals(value))
+        {
+          current.Prev = current.Prev.Prev;
+        }
+
+      }
+    }
+
+    /// <summary>
+    /// Relace the value at the position of the index with the given value
+    /// </summary>
+    /// <param name="index"></param>
+    /// <param name="value"></param>
+    public void Replace(int index, T value)
+    {
+      Node nodetoAd = new Node
+      {
+        Data = value
+      };
+
+      int i = 0;
+      for (Node current = _topElement; current != null; current = current.Next)
+      {
+        if (index == i && i == 0)
+        {
+          nodetoAd.Next = current.Next;
+          nodetoAd.Next.Prev = nodetoAd;
+          _topElement = nodetoAd;
+        }
+        else if (index == i)
+        {
+          nodetoAd.Next = current.Next;
+          nodetoAd.Next.Prev = nodetoAd;
+          nodetoAd.Prev = current.Prev;
+          current.Prev.Next = nodetoAd;
+        }
+        i++;
+      }
+    }
+
+    /// <summary>
+    /// Insert a value into the list
+    /// </summary>
+    /// <param name="index"></param>
+    /// <param name="value"></param>
+    public void Insert(int index, T value)
+    {
+      Node nodetoAd = new Node
+      {
+        Data = value
+      };
+
+      int i = 0;
+      for (Node current = _topElement; current != null; current = current.Next)
+      {
+        if (index == i && i == 0)
+        {
+          nodetoAd.Next = current;
+          nodetoAd.Next.Prev = nodetoAd;
+          _topElement = nodetoAd;
+        }
+        else if (index == i)
+        {
+          nodetoAd.Next = current;
+          nodetoAd.Prev = current.Prev;
+          nodetoAd.Next.Prev = nodetoAd;
+          nodetoAd.Prev.Next = nodetoAd;
+          current = nodetoAd;
+        }
+        i++;
+      }
+    }
+  }
+}
---
+++ DoublyLinkedList/Test/Test.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using DoubleLinkedList;
+namespace Test
+{
+  class Test
+  {
+    [TestFixture]
+    class Test1
+    {
+      /// <summary>
+      /// Check if the given values are added at the end of the list
+      /// </summary>
+      [Test]
+      public void TestAdd()
+      {
+        // Given 
+        DoublyLinkedList<int> list = new DoublyLinkedList<int>();
+        list.Add(1);
+        list.Add(2);
+        // When /Then
+        Assert.That(list.Get(0), Is.EqualTo(1));
+        Assert.That(list.Get(1), Is.EqualTo(2));
+        Assert.That(list.Length(), Is.EqualTo(2));
+      }
+      [Test]
+      public void TestAddString()
+      {
+        // Given 
+        DoublyLinkedList<string> listString = new DoublyLinkedList<string>();
+        listString.Add("1");
+        listString.Add("2");
+        // When /Then
+        Assert.That(listString.Get(0), Is.EqualTo("1"));
+        Assert.That(listString.Get(1), Is.EqualTo("2"));
+        Assert.That(listString.Length(), Is.EqualTo(2));
+      }
+
+      /// <summary>
+      /// Check if the given Values exists
+      /// </summary>
+      [Test]
+      public void TestExists()
+      {
+        // Given 
+        DoublyLinkedList<int> list = new DoublyLinkedList<int>();
+        list.Add(1);
+        list.Add(2);
+        // When /Then
+        Assert.That(list.Exists(1), Is.EqualTo(true));
+        Assert.That(list.Exists(2), Is.EqualTo(true));
+        Assert.That(list.Exists(5), Is.EqualTo(false));
+      }
+      /// <summary>
+      /// Check if the given Values exists
+      /// </summary>
+      [Test]
+      public void TestExistsString()
+      {
+        // Given 
+        DoublyLinkedList<int> list = new DoublyLinkedList<int>();
+        list.Add(1);
+        list.Add(2);
+        // When /Then
+        Assert.That(list.Exists(1), Is.EqualTo(true));
+        Assert.That(list.Exists(2), Is.EqualTo(true));
+        Assert.That(list.Exists(5), Is.EqualTo(false));
+      }
+
+      // <summary>
+      /// Check if the arrays length is equal to the given length
+      /// </summary>
+      [Test]
+      public void TestLength()
+      {
+        // Given
+        DoublyLinkedList<int> list = new DoublyLinkedList<int>();
+        list.Add(1);
+        list.Add(2);
+        // When /Then
+        Assert.That(list.Length(), Is.EqualTo(2));
+      }
+
+      // <summary>
+      /// Check if the arrays length is equal to the given length
+      /// </summary>
+      [Test]
+      public void TestLengthString()
+      {
+        // Given
+        DoublyLinkedList<string> list = new DoublyLinkedList<string>();
+        list.Add("1");
+        list.Add("2");
+        // When /Then
+        Assert.That(list.Length(), Is.EqualTo(2));
+      }
+
+      /// <summary>
+      /// Check if the get function returns the correct values from the list
+      /// </summary>
+      [Test]
+      public void TestGet()
+      {
+        // Given
+        DoublyLinkedList<int> list = new DoublyLinkedList<int>();
+        list.Add(1);
+        list.Add(2);
+        // When /Then
+        Assert.That(list.Get(0), Is.EqualTo(1));
+        Assert.That(list.Get(1), Is.EqualTo(2));
+        Assert.That(() => list.Get(-1), Throws.ArgumentException);
+        Assert.That(() => list.Get(2), Throws.ArgumentException);
+      }
+
+      /// <summary>
+      /// Check if the get function returns the correct values from the list
+      /// </summary>
+      [Test]
+      public void TestGetString()
+      {
+        // Given
+        DoublyLinkedList<string> list = new DoublyLinkedList<string>();
+        list.Add("1");
+        list.Add("2");
+        // When /Then
+        Assert.That(list.Get(0), Is.EqualTo("1"));
+        Assert.That(list.Get(1), Is.EqualTo("2"));
+        Assert.That(() => list.Get(-1), Throws.ArgumentException);
+        Assert.That(() => list.Get(2), Throws.ArgumentException);
+      }
+
+      /// <summary>
+      /// Check if the given Value at the beginning is removed from the list
+      /// </summary>
+      [Test]
+      public void TestRemoveAtBeginning()
+      {
+        // Given
+        DoublyLinkedList<int> list = new DoublyLinkedList<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        // When
+        list.Remove(1);
+        // Then
+        Assert.That(list.Length(), Is.EqualTo(2));
+        Assert.That(list.Get(0), Is.EqualTo(2));
+        Assert.That(list.Get(1), Is.EqualTo(3));
+      }
+
+      /// <summary>
+      /// Check if the given Value at the beginning is removed from the list
+      /// </summary>
+      [Test]
+      public void TestRemoveAtBeginningString()
+      {
+        // Given
+        DoublyLinkedList<string> list = new DoublyLinkedList<string>();
+        list.Add("1");
+        list.Add("2");
+        list.Add("3");
+        // When
+        list.Remove("1");
+        // Then
+        Assert.That(list.Length(), Is.EqualTo(2));
+        Assert.That(list.Get(0), Is.EqualTo("2"));
+        Assert.That(list.Get(1), Is.EqualTo("3"));
+      }
+
+      /// <summary>
+      /// Check if the given Value in the middle is removed from the list
+      /// </summary>
+      [Test]
+      public void TestRemoveAtMiddle()
+      {
+        // Given
+        DoublyLinkedList<int> list = new DoublyLinkedList<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        // When
+        list.Remove(2);
+        // Then
+        Assert.That(list.Length(), Is.EqualTo(2));
+        Assert.That(list.Get(0), Is.EqualTo(1));
+        Assert.That(list.Get(1), Is.EqualTo(3));
+      }
+
+      /// <summary>
+      /// Check if the given Value in the middle is removed from the list
+      /// </summary>
+      [Test]
+      public void TestRemoveAtMiddleString()
+      {
+        // Given
+        DoublyLinkedList<string> list = new DoublyLinkedList<string>();
+        list.Add("1");
+        list.Add("2");
+        list.Add("3");
+        // When
+        list.Remove("2");
+        // Then
+        Assert.That(list.Length(), Is.EqualTo(2));
+        Assert.That(list.Get(0), Is.EqualTo("1"));
+        Assert.That(list.Get(1), Is.EqualTo("3"));
+      }
+
+      /// <summary>
+      /// Check if the given Value at the end is removed from the list
+      /// </summary>
+      [Test]
+      public void TestRemoveAtEnd()
+      {
+        // Given
+        DoublyLinkedList<int> list = new DoublyLinkedList<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        // When
+        list.Remove(3);
+        // Then
+        Assert.That(list.Length(), Is.EqualTo(2));
+        Assert.That(list.Get(0), Is.EqualTo(1));
+        Assert.That(list.Get(1), Is.EqualTo(2));
+      }
+
+      /// <summary>
+      /// Check if the given Value at the end is removed from the list
+      /// </summary>
+      [Test]
+      public void TestRemoveAtEndString()
+      {
+        // Given
+        DoublyLinkedList<string> list = new DoublyLinkedList<string>();
+        list.Add("1");
+        list.Add("2");
+        list.Add("3");
+        // When
+        list.Remove("3");
+        // Then
+        Assert.That(list.Length(), Is.EqualTo(2));
+        Assert.That(list.Get(0), Is.EqualTo("1"));
+        Assert.That(list.Get(1), Is.EqualTo("2"));
+      }
+
+      [Test]
+      public void TestReplace()
+      {
+        //Given
+        DoublyLinkedList<int> list = new DoublyLinkedList<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        //When
+        list.Replace(index: 0, value: 4);
+        list.Replace(index: 1, value: 5);
+        //Then
+        Assert.That(list.Get(0), Is.EqualTo(4));
+        Assert.That(list.Get(1), Is.EqualTo(5));
+        Assert.That(list.Get(2), Is.EqualTo(3));
+      }
+
+      [Test]
+      public void TestReplaceString()
+      {
+        //Given
+        DoublyLinkedList<string> list = new DoublyLinkedList<string>();
+        list.Add("1");
+        list.Add("2");
+        list.Add("3");
+        //When
+        list.Replace(index: 0, value: "4");
+        list.Replace(index: 1, value: "5");
+        //Then
+        Assert.That(list.Get(0), Is.EqualTo("4"));
+        Assert.That(list.Get(1), Is.EqualTo("5"));
+        Assert.That(list.Get(2), Is.EqualTo("3"));
+      }
+
+      [Test]
+      public void TestInsert()
+      {
+        // Given
+        DoublyLinkedList<int> list = new DoublyLinkedList<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        // When
+        list.Insert(index: 0, value: 5);
+        list.Insert(index: 2, value: 4);
+        // Then
+        Assert.That(list.Length(), Is.EqualTo(5));
+        Assert.That(list.Get(0), Is.EqualTo(5));
+        Assert.That(list.Get(1), Is.EqualTo(1));
+        Assert.That(list.Get(2), Is.EqualTo(4));
+        Assert.That(list.Get(3), Is.EqualTo(2));
+        Assert.That(list.Get(4), Is.EqualTo(3));
+      }
+
+      [Test]
+      public void TestInsertString()
+      {
+        // Given
+        DoublyLinkedList<string> list = new DoublyLinkedList<string>();
+        list.Add("1");
+        list.Add("2");
+        list.Add("3");
+        // When
+        list.Insert(index: 0, value: "5");
+        list.Insert(index: 2, value: "4");
+        // Then
+        Assert.That(list.Length(), Is.EqualTo(5));
+        Assert.That(list.Get(0), Is.EqualTo("5"));
+        Assert.That(list.Get(1), Is.EqualTo("1"));
+        Assert.That(list.Get(2), Is.EqualTo("4"));
+        Assert.That(list.Get(3), Is.EqualTo("2"));
+        Assert.That(list.Get(4), Is.EqualTo("3"));
+      }
+    }
+  }
+}
